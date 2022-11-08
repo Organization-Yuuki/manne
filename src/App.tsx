c76c4f72e463e58d5ee483a61576659329e3f3eb
kira-0521 <kiratanaka0521@gmail.com>
2022-11-08 19:52:24 +0900

    [ add ] buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { FC } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
-import './App.css'
+
 import { ErrorFallback } from './components/errors/ErrorFallback'
+import { SocialButton } from './components/parts/buttons/SocialButton'
 
 const App: FC = () => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <div className="App">Hello manne!!!!!</div>
+      {/* NOTE: 仮 */}
+      <div style={{ padding: '10px' }}>
+        <div className="App">Hello manne!!!!!</div>
+        <br />
+        <SocialButton type="google" />
+        <br />
+        <SocialButton type="facebook" />
+        <br />
+        <SocialButton type="apple" />
+      </div>
     </ErrorBoundary>
   )
 }
