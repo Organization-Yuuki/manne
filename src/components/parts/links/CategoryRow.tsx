9f7540186f752013d2ce1cccb223ee7677a70d44
kira-0521 <kiratanaka0521@gmail.com>
2022-11-27 12:08:25 +0900

    [ feature ] カテゴリーリストの１パーツ

--- src/components/parts/links/CategoryRow.tsx
+++ src/components/parts/links/CategoryRow.tsx
@@ -1,5 +1,24 @@
-import { FC } from 'react'
+import { FC, memo } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import clsx from 'clsx'
 
-export const CategoryRow: FC = () => {
-  return <div></div>
+import { commonIconStyle } from '../../../styles/parts/icon.style'
+import { commonRowStyle } from '../../../styles/parts/row.style'
+
+interface Props {
+  url: string
+  imgUrl: string
+  categoryName: string
 }
+
+const BeforeMemoCategoryRow: FC<Props> = ({ url, imgUrl, categoryName }) => {
+  return (
+    <RouterLink to={url} css={commonRowStyle} className={clsx('category')}>
+      <img src={imgUrl} alt={categoryName} css={commonIconStyle} />
+      <span>{categoryName}</span>
+    </RouterLink>
+  )
+}
+
+export const CategoryRow: FC<Props> = memo(BeforeMemoCategoryRow)
+CategoryRow.displayName = 'CategoryRow'
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,5 +1,16 @@
 import { FC } from 'react'
 
+import { CategoryRow } from '../components/parts/links/CategoryRow'
+import sample from '../assets/images/sample_icon.png'
+
 export const ProductList: FC = () => {
-  return <div>ProductList</div>
+  return (
+    <div>
+      <CategoryRow
+        url="/auth"
+        categoryName="カテゴリー名を挿入"
+        imgUrl={sample}
+      />
+    </div>
+  )
 }
