3152e7812c3088339258f7a7610e99700a43c464
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2022-12-14 21:22:34 +0900

    add isValidPhoneNumber

--- src/lib/utils/validate.ts
+++ src/lib/utils/validate.ts
@@ -1,11 +1,15 @@
 import { isNil } from 'lodash'
 
-// - 36文字以内
+// - 10文字以上36文字以内
 // - 数字
 // - null or undefinedじゃない
 export const isValidPhoneNumber = (
-  inputPhoneNumber: string | number | undefined
+  phoneNumber: string | number | undefined
 ): boolean => {
-  if (isNil(inputPhoneNumber)) return false
+  if (isNil(phoneNumber)) return false
+  const length = phoneNumber.toString().length
+  const isValidLength = length >= 10 && length <= 36
+  if (!isValidLength) return false
+  if (typeof phoneNumber === 'string') return false
   return true
 }
